f81d70124b271743fd3506381e5ead5ee506a2ec
Fu_Qingchen <fuqingchen.gov@gmail.com>
2021-02-21 19:38:16 +0800

    更新：第三周计划

---
+++ Source/705.设计哈希集合.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=705 lang=cpp
+ *
+ * [705] 设计哈希集合
+ */
+
+// @lc code=start
+class MyHashSet {
+public:
+    /** Initialize your data structure here. */
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+
+    }
+    
+    void remove(int key) {
+
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
+// @lc code=end
+
