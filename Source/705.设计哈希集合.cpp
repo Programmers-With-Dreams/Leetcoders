6cdc8ace9dd41ed0296b2ed1a87e4f619f822cdd
Fu_Qingchen <fuqingchen.gov@gmail.com>
2021-02-28 19:33:36 +0800

    完成：哈希

--- Source/705.设计哈希集合.cpp
+++ Source/705.设计哈希集合.cpp
@@ -5,30 +5,37 @@
  */
 
 // @lc code=start
+#include<array>
+using namespace std;
 class MyHashSet {
 public:
     /** Initialize your data structure here. */
-    MyHashSet() {
-        datas = new int[10000];
-    }
-
-    ~MyHashSet() { delete [] datas; }
+    MyHashSet() { length = 10007; for (auto &item : datas) { item = -1; } }
     
-    void add(int key) {
-
-    }
+    void add(int key) { datas.at(search(key)) = key; }
     
-    void remove(int key) {
-
-    }
+    void remove(int key) { datas.at(search(key)) = -2; } //由于remove，可能导致中间会出现空槽
     
     /** Returns true if this set contains the specified element */
     bool contains(int key) {
-
+        if (datas.at(search(key)) < 0) { return false; }
+        else { return true; }
     }
+
 private:
-    int *datas;
-    int hash(int key);
+    array<int, 10007> datas;
+    int length = 10007;
+    int hash(int key) { return ((key * 2 + 1 + length) % length); }
+    int search(int key) {
+        int hash_value = hash(key), flag = 1, mul = 1, index = hash_value;
+        while (datas.at(index) != -1) {
+            if(datas.at(index) == key) { return index; }
+            index = (hash_value + flag * mul + length) % length;
+            if (flag == -1) { ++mul; }
+            flag *= -1;
+        }
+        return index;
+    }   // 查找失败返回下一个空位置的 index
 };
 
 /**
